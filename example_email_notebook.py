e04942faf6ae62b592dcf1544e687b0a4a7fcf5f
Thomas Jack Carroll <thomas.jack.carroll@gmail.com>
2021-08-30 06:43:04 -0400

    Create example_email_notebook.py

---
+++ example_email_notebook.py
@@ -0,0 +1,52 @@
+"""
+This is an example DAG which uses Papermill Operator, Python Operator, 
+and Email Operator to produce and email reports to various users.
+First, the Papermill operator executes a templated notebook,
+then the Python operator runs nbconvert to produce an output artifact from
+that templated notebook as a pdf. Finally, an email is crafted and sent
+with the output pdf attached.
+"""
+
+from datetime import datetime, timedelta
+
+# [START import_module]
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.operators import PapermillOperator, PythonOperator, EmailOperator, BashOperator
+
+# [END import_module]
+
+
+# [START instantiate_dag]
+default_args={
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'max_active_runs': "1",
+    'tags': ['example', 'notebook'],
+}
+
+dag = DAG(
+    'email_notebook',
+    default_args=default_args,
+    description='example to execute and email notebook to stakeholders',
+    schedule_interval='@daily',
+    start_date=datetime(2021, 8, 28),
+    catchup=False,
+)
+
+run_this = BashOperator(
+    task_id='run_this',
+    bash_command='echo 1',
+)
+
+then_run_this = BashOperator(
+    task_id='run_this',
+    bash_command='echo 2',
+)
+
+run_this >> then_run_this
